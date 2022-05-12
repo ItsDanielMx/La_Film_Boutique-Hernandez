c99f5f4d1891ebdf5c4fbe095ba0f189057cd3b6
Daniel Hernandez <danielhernandez09012002@gmail.com>
2022-05-12 17:00:01 -0500

    Pagina Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './Components/Navegacion/Navbar'
+import Inicio from './Components/Pages/Inicio'
+import Contacto from './Components/Pages/Contacto'
+import Productos from './Components/Pages/Productos'
+import AboutUs from './Components/Pages/AboutUs'
+import Login from './Components/Pages/Login';
+import SignUp from './Components/Pages/SignUp';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact components={Inicio}/>
+          <Route path='/Contacto' exact components={Contacto}/>
+          <Route path='/Productos' exact components={Productos}/>
+          <Route path='/AboutUs' exact components={AboutUs}/>
+          <Route path='/Login' exact components={Login}/>
+          <Route path='/SignUp' exact components={SignUp}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
