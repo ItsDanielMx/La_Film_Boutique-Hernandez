ecb3358a55de855cc6801a8b0900d04f72eddfbb
Daniel Hernandez <danielhernandez09012002@gmail.com>
2022-06-16 13:58:42 -0500

    footer href

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
   return (
     <footer id="footer">
         <div className="redes">
-            <a src="https://www.facebook.com/Lafilmboutique" target='_blank' rel="noopener noreferrer" className='mx-3'><FacebookIcon /></a>
-            <a src="https://www.instagram.com/lafilmboutique/" target='_blank' rel="noopener noreferrer" className='mx-3'><InstagramIcon /></a>
-            <a src="https://www.pinterest.com/lafilmboutique" target='_blank' rel="noopener noreferrer" className='mx-3'><PinterestIcon /></a>
+            <a href="https://www.facebook.com/Lafilmboutique" target='_blank' rel="noopener noreferrer" className='mx-3'><FacebookIcon /></a>
+            <a href="https://www.instagram.com/lafilmboutique/" target='_blank' rel="noopener noreferrer" className='mx-3'><InstagramIcon /></a>
+            <a href="https://www.pinterest.com/lafilmboutique" target='_blank' rel="noopener noreferrer" className='mx-3'><PinterestIcon /></a>
         </div>
         <div className="mb-3"> <br />
             <span className="d-flex justify-content-between">
