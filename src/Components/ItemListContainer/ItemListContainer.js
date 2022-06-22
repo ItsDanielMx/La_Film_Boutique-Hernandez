5d3667eb95c0697e399bae64774cbb2d0dbb1dc8
Daniel Hernandez <danielhernandez09012002@gmail.com>
2022-06-22 12:03:05 -0500

    Firestore

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import ItemList from './ItemList'
-import products from '../Utils/Products'
 import { useParams } from 'react-router-dom'
 import { firestoreFetch } from '../Utils/FirestoreFetch';
 
 
 const ItemListContainer = ({greeting}) => {
     const [datos, setDatos] = useState([])
-    const [loading, setLoading] = useState(false)
     const { idCategory } = useParams();
 
     //componentDidUpdate
@@ -28,9 +26,7 @@ const ItemListContainer = ({greeting}) => {
             <section className="camaras container">
             <h1 className="mt-4 tituloProductos">{greeting}</h1>
             <hr className="container"></hr>
-            {loading ? <div className="loading show">
-            <div className="spin"></div>
-            </div> : <ItemList items={datos}/>}
+            <ItemList items={datos}/>
 
         </section>
     )
